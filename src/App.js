af386a58312ce54c884785126f0d7bbbf0418cd9
Turatali <tataovo877@gmail.com>
2025-05-06 08:51:42 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import Header from "./components/header/Header";
-import Swagger from "../src/swagger/Swagger"
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Swagger/>
     </div>  
   );
 }
