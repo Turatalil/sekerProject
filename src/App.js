a084f9dfbd74464014f7dcc2bf970bb959e73d5c
Beknazara <orozmamatovabeknazara@gmail.com>
2025-05-06 14:52:08 +0600

    only

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import LikeButton from "./UI/LikeButton";
-import Select from "./UI/Select";
-
 function App() {
   return (
-    <div className="App">
-      <LikeButton/>
-      <Select/>
-    </div>  
+    <div>
+
+    </div>
   );
 }
 
