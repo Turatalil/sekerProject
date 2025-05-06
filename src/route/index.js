785c1a63cd162f38ab386472a4761bf6f0b1f90e
Turatali <tataovo877@gmail.com>
2025-05-06 09:42:01 +0300

    admin user

--- src/route/index.js
+++ src/route/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Header from '../components/header/Header'
 import Swagger from '../swagger/Swagger'
+import UserPage from '../pages/UserPage'
+
 
 const MainRoute = () => {
     return (
         <Routes>
             <Route path="/admin"  element={<><Header /><Swagger /></>}/>
-            <Route path="user" />
+            <Route path="/user" element={<UserPage/>}/>
         </Routes>
     )
 }
